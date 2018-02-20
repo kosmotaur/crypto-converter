31224dc8b6ee7002687bdc855de23d3a6dc704e5
Marek Stasikowski <marek.stasikowski@me.com>
2018-02-20 23:29:48 +0000

    WIP

--- src/argv.js
+++ src/argv.js
@@ -2,9 +2,11 @@ import yargs from 'yargs'
 
 export default () =>
   yargs
+    .usage('$0 [options] [path-to-balances.json]')
     .option('currency', {
       alias: 'c',
       default: 'gbp',
       description: 'Target currency to convert holdings to'
     })
+    .example('$0 balances.json')
     .argv
