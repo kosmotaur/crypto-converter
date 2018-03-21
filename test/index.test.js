a839fc960134f2191d9c3f6234efdd5b667a94a7
Marek Stasikowski <marek.stasikowski@me.com>
2018-03-21 07:25:01 +0000

    WIP

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+import test from 'ava'
+import validBalances from './helpers/validBalances'
+import td from 'testdouble'
+
+test.before(() => {
+  td.replace()
+})
+test('calls Cryptocompare API', t => {
+
+})
